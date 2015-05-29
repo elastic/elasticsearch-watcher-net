30c22e9c7e3256ab7ff6d62c221096a53d09fdf5
Martijn Laarman <mpdreamz@gmail.com>
2015-05-29 11:23:10 +0200

    Moved over elasticsearch-net build infrastructure and adopted it for the watcher repos, build and build release work as expected on the commandline with this commit but we still need to move to paket so we can sanely build 4.0 and 4.5 projects using msbuild. This is pending the NEST 1.5.1 release.

---
+++ build.bat
@@ -0,0 +1,36 @@
+@echo off
+REM we need nuget to install tools locally
+if not exist build\tools\nuget\nuget.exe (
+    ECHO Nuget not found.. Downloading..
+    mkdir build\tools\nuget
+    PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& 'build\download-nuget.ps1'"
+)
+
+REM we need FAKE to process our build scripts
+if not exist build\tools\FAKE\tools\Fake.exe (
+    ECHO FAKE not found.. Installing..
+    "build\tools\nuget\nuget.exe" "install" "FAKE" "-OutputDirectory" "build\tools" "-Version" "3.28.8" "-ExcludeVersion"
+)
+
+if not exist build\tools\gitlink\lib\net45\gitlink.exe (
+    ECHO Local gitlink not found.. Installing..
+    "build\tools\nuget\nuget.exe" "install" "gitlink" "-OutputDirectory" "build\tools" "-ExcludeVersion" "-Prerelease"
+)
+
+REM we need nunit-console to run our tests
+if not exist build\tools\NUnit.Runners\tools\nunit-console.exe (
+    ECHO Nunit not found.. Installing
+    "build\tools\nuget\nuget.exe" "install" "NUnit.Runners" "-OutputDirectory" "build\tools" "-ExcludeVersion" "-Prerelease"
+)
+
+SET TARGET="Build"
+SET VERSION=
+
+IF NOT [%1]==[] (set TARGET="%1")
+
+IF NOT [%2]==[] (set VERSION="%2")
+
+shift
+shift
+
+"build\tools\FAKE\tools\Fake.exe" "build\\scripts\\build.fsx" "target=%TARGET%" "version=%VERSION%"
